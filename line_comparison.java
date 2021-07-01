72a71d5e5dc93c3f6b687bf8cc6fa95c78030ff8
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-01 12:09:45 +0530

    Added code to check whether 2 lines are equal or not

--- line_comparison.java
+++ line_comparison.java
@@ -1,12 +1,25 @@
 public class line_comparison {
 public static void main(String args[])
-{int x1=3;
+{
+int x1=3;
 int y1=5;
 int x2=9;
 int y2=7;
-double len;
+int x3=6;
+int y3=7;
+int x4=2;
+int y4=5;
+double len1, len2, line_equal;
 System.out.println("Welcome to Line Comparison Computation Program");
-len = Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
-System.out.println(len);
+len1 = Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+len2 = Math.sqrt(Math.pow((x4-x3), 2)+Math.pow((y4-y3),2));
+String s1 = String.valueOf(len1);
+String s2 = String.valueOf(len2);
+if(s2.equals(s1))
+	{
+		System.out.println("Lines are equal");
+	}
+else
+	System.out.println("Lines are not equal");
 }
 }
