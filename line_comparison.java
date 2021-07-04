9bdf2dfc51e15694141eab14a8d0c03df94db984
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-04 10:57:26 +0530

    Refactored code to check whether 2 lines are equal or not

--- line_comparison.java
+++ line_comparison.java
@@ -1,25 +1,39 @@
+import java.util.Scanner;
 public class line_comparison {
-public static void main(String args[])
-{
-int x1=3;
-int y1=5;
-int x2=9;
-int y2=7;
-int x3=6;
-int y3=7;
-int x4=2;
-int y4=5;
-double len1, len2, line_equal;
-System.out.println("Welcome to Line Comparison Computation Program");
-len1 = Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
-len2 = Math.sqrt(Math.pow((x4-x3), 2)+Math.pow((y4-y3),2));
-String s1 = String.valueOf(len1);
-String s2 = String.valueOf(len2);
-if(s2.equals(s1))
-	{
-		System.out.println("Lines are equal");
+
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int x1, x2,x3,x4,y1,y2,y3,y4;
+		double len1, len2,line_equal;
+		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Enter the first point of line1");
+		x1=sc.nextInt();
+		System.out.println("Enter the second point of line1");
+                x2=sc.nextInt();
+		System.out.println("Enter the third point of line1");
+                y1=sc.nextInt();
+		System.out.println("Enter the fourth number of line1");
+                y2=sc.nextInt();
+		System.out.println("Enter the first point of line2");
+                x3=sc.nextInt();
+                System.out.println("Enter the second point of line2");
+                x4=sc.nextInt();
+                System.out.println("Enter the third point of line2");
+                y3=sc.nextInt();
+                System.out.println("Enter the fourth point of line2");
+                y4=sc.nextInt();
+                sc.close();
+		len1 = Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+		len2 = Math.sqrt(Math.pow((x4-x3), 2)+Math.pow((y4-y3),2));
+		String s1 = String.valueOf(len1);
+		String s2 = String.valueOf(len2);
+		if(s2.equals(s1)) {
+	                System.out.println("Lines are equal");
+		}
+		else
+	        System.out.println("Lines are not equal");
+		
+
 	}
-else
-	System.out.println("Lines are not equal");
 }
-}
+
